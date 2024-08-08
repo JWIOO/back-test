a710c0be513235afe6db6d6cf72985893df8e11a
JIWOO <167792693+JWIOO@users.noreply.github.com>
2024-08-08 16:21:56 +0900

    Delete applicationform directory

--- applicationform/repository/ApplicationFormRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.aurum.educerti.certificate.repository
-
-import com.aurum.educerti.applicationform.domain.ApplicationForm
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ApplicationFormRepository : JpaRepository<ApplicationForm, Long> {
-    fun findByMemberId(memberId: Long): List<ApplicationForm>
-}
