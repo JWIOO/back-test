e4ac62e4753f4ecb7ecc8997aba5b9fd4f9d0e81
JIWOO <167792693+JWIOO@users.noreply.github.com>
2024-08-08 16:22:14 +0900

    Delete examsession directory

--- examsession/repository/ExamSessionRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.aurum.educerti.certificate.repository
-
-import com.aurum.educerti.examsession.domain.ExamSession
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ExamSessionRepository : JpaRepository<ExamSession, Long> {
-    fun findByExamCode(examCode : String) : ExamSession?
-}
